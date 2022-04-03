c8e992b500f81067e0d9e73a01446f73755d6e94
Nick Wakefield <n.g.wakefield@gmail.com>
2022-04-03 11:14:05 -0500

    Start inserting code

--- utilities.ps1
+++ utilities.ps1
@@ -5,4 +5,8 @@ function Make-Twoway {
 # Prompts for two directories
 # Creates links form directory 1 to directory 2 and vice versa
 
+# Prompt for directory 1
+$dir1 = Read-Host -Prompt 'What is the first directory you want to link?'
+# Prompt for directory 2
+$dir1 = Read-Host -Prompt 'What is the second directory you want to link?'
 }
